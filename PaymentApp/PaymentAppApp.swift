1df41e50c6caa0a5774cff4ede5281dd45b2e93e
Vinnicius Pereira <pereiradasilva0@hotmail.com>
2025-01-20 10:37:57 -0300

    Initial Commit

---
+++ PaymentApp/PaymentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaymentAppApp.swift
+//  PaymentApp
+//
+//  Created by Vinnicius Pereira on 20/01/25.
+//
+
+import SwiftUI
+
+@main
+struct PaymentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
